cf6ee3d6dfb4b22c3761e235296ef94550492e4b
Wen Wu <wen_wu@bizlinktech.com>
2021-03-15 14:46:31 +0800

    Add a state design pattern

---
+++ FiniteStateMachine/FiniteStateMachine/FiniteStateMachine.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteStateMachine
+{
+    class FiniteStateMachine
+    {
+
+        enum internalState
+        {
+            SmallMario,
+            SuperMario,
+            FireMario,
+            CapMario
+        }
+        private internalState State { get; set; }
+        public int LifeCount { get; private set; }
+        public int CoinCount { get; private set; }
+
+        public FiniteStateMachine()
+        {
+            LifeCount = 1;
+            CoinCount = 0;
+            State = internalState.SmallMario;
+        }
+        public void GotMushroom()
+        {
+            Console.WriteLine("Got Mushroom");
+            if (State == internalState.SmallMario)
+            {
+                State = internalState.SuperMario;
+            }
+            GotCoins(100);
+        }
+        public void GotFireFlower()
+        {
+            Console.WriteLine("Got FireFlower!");
+            State = internalState.FireMario;
+            GotCoins(200);
+        }
+        public void GotFeather()
+        {
+            Console.WriteLine("Got Feather!");
+            State = internalState.CapMario;
+            GotCoins(300);
+        }
+        public void GotCoins(int numberOfCoins)
+        {
+            Console.WriteLine($"Got {numberOfCoins} Coin(s)!");
+            CoinCount += numberOfCoins;
+            if (CoinCount >= 5000)
+            {
+                GotLife();
+                CoinCount -= 5000;
+            }
+        }
+        public void GotLife()
+        {
+            Console.WriteLine("Got Life!");
+            LifeCount += 1;
+        }
+        public void LostLife()
+        {
+            Console.WriteLine("Lost Life!");
+            LifeCount -= 1;
+            if (LifeCount <= 0)
+            {
+                GameOver();
+            }
+        }
+        public void MetMonster()
+        {
+            Console.WriteLine("Met Monster!");
+            if (State == internalState.SmallMario)
+            {
+                LostLife();
+            }
+            else
+            {
+                State = internalState.SmallMario;
+            }
+        }
+        public void GameOver()
+        {
+            LifeCount = 0;
+            CoinCount = 0;
+            Console.WriteLine("Game Over!");
+        }
+        public override string ToString()
+        {
+            return $"State: {State} | LifeCount: {LifeCount} | CoinsCount: {CoinCount} \n";
+        }
+    }
+}
---
+++ FiniteStateMachine/FiniteStateMachine/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteStateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FiniteStateMachine state = new FiniteStateMachine();
+            Console.WriteLine(state);
+
+            state.GotMushroom();
+            Console.WriteLine(state);
+
+            state.GotFireFlower();
+            Console.WriteLine(state);
+
+            state.GotFeather();
+            Console.WriteLine(state);
+
+            state.GotCoins(4800);
+            Console.WriteLine(state);
+
+            state.MetMonster();
+            Console.WriteLine(state);
+
+            state.MetMonster();
+            Console.WriteLine(state);
+
+            state.MetMonster();
+            Console.WriteLine(state);
+            Console.ReadLine();
+        }
+    }
+}
